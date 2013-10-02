7d4df54b1408ca04de0e75a5051c124595a9b4fd
obviousdeception <git@obviousdeception.com>
2013-10-01 21:10:40 -0700

    Added URL checking, cleaned up formatting

--- background.js
+++ background.js
@@ -1,40 +1,36 @@
 //based on "Page action by URL" example at:
 //http://developer.chrome.com/extensions/samples.html#
 
-
 function isUrlNewTab(url){
-    return true;
+	if( url ){
+		return url.match(/google\.com\/webhp\?sourceid=chrome-instant/g) ;
+	}
+    return false;
 }
 
 function isTitleNewTab(title){
-    return (title == "New Tab");
+	return (title == "New Tab");
 }
 
 function processTabAction(tab){
- //check to make sure this is the New Tab page
- if( isUrlNewTab(tab.url) && isTitleNewTab(tab.title) ){
-    // alert(tab.status);
-     //hide the most-visited section
-     chrome.tabs.executeScript(tab.id, {code:"document.getElementById('most-visited').style.display = 'none'"});
- //alert(tab.url);
- }
-};
+	//check to make sure this is the New Tab page
+	if( isUrlNewTab(tab.url) && isTitleNewTab(tab.title) ){
+		//hide the most-visited section
+		chrome.tabs.executeScript(tab.id, {code:"document.getElementById('most-visited').style.display = 'none'"});
 
+	}
+};
 
 // Called when the url of a tab changes.
 function handleTabUrlChange(tabId, changeInfo, tab) { 	
-  processTabAction(tab);
+	processTabAction(tab);
 };
 
 // Called when the new tab is created.
 function handleNewTabCreated(tab) {
-	
- processTabAction(tab);
+	processTabAction(tab);
 };
-
-
 
 // Listen for any changes to the URL of any tab.
 chrome.tabs.onUpdated.addListener(handleTabUrlChange);
-
 chrome.tabs.onCreated.addListener(handleNewTabCreated);
